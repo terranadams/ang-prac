1ae1a4922c90fcbfad0280b6ed4c374855cc0ff6
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-12-28 13:05:54 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonsComponent } from './components/persons/persons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsComponent
   ],
   imports: [
     BrowserModule

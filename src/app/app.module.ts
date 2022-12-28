220d33f07ca475650c420e85f5be65084fd06822
Terran Adams <terranadams@Terrans-MacBook-Air.local>
2022-12-28 13:31:10 -0700

    made input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PersonsComponent } from './components/persons/persons.component';
+import { PersonInputComponent } from './components/person-input/person-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PersonsComponent
+    PersonsComponent,
+    PersonInputComponent
   ],
   imports: [
     BrowserModule
